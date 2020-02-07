6609c105a1fa788fbdf0cdd0897950d3e1c78f6f
Makisto54 <ilja-martasov@mail.ru>
2020-02-07 10:36:37 +0700

    Add dynamic libs in program v1.0

--- Makefile
+++ Makefile
@@ -1,19 +1,27 @@
 GCC = gcc -Wall -Werror
-OBJ = build/add.o build/sub.o build/div.o build/mul.o
 
 .PHONY: clean 
 
-all: build lib default
+all: build lib libs default
 
-lib: libmycomplex.so
+libs: libs/libadd.so libs/libsub.so libs/libdiv.so libs/libmul.so
 
 default: mycomplex
 
-libmycomplex.so: $(OBJ)
-	$(GCC) -shared -o libmycomplex.so $(OBJ)
+mycomplex: src/main.c src/mycomplex.h
+	$(GCC) src/main.c -o mycomplex -ldl
+		
+libs/libadd.so: build/add.o
+	$(GCC) -shared -o libs/libadd.so build/add.o
 
-mycomplex: build/main.o $(OBJ)
-	$(GCC) build/main.o -o mycomplex -L. -l mycomplex
+libs/libsub.so: build/sub.o
+	$(GCC) -shared -o libs/libsub.so build/sub.o
+
+libs/libdiv.so: build/div.o
+	$(GCC) -shared -o libs/libdiv.so build/div.o
+
+libs/libmul.so: build/mul.o
+	$(GCC) -shared -o libs/libmul.so build/mul.o
 
 build/add.o: src/add.c src/mycomplex.h
 	$(GCC) -fPIC -c src/add.c -o build/add.o
@@ -27,11 +35,11 @@ build/mul.o: src/mul.c src/mycomplex.h
 build/div.o: src/div.c src/mycomplex.h
 	$(GCC) -fPIC -c src/div.c -o build/div.o
 
-build/main.o: src/main.c src/mycomplex.h
-	$(GCC) -c src/main.c -o build/main.o
-
 build:
 	mkdir build
 
+lib:
+	mkdir libs
+
 clean: 
-	rm -rf build libmycomplex.so mycomplex
+	rm -rf build libs mycomplex
--- src/main.c
+++ src/main.c
@@ -1,67 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dlfcn.h>
 #include "mycomplex.h"
 
 int main(int argc, char* argv[])
 {
+    system("clear");
+    int n = 0;
     struct mycomplex* a = malloc(sizeof(struct mycomplex));
     struct mycomplex* b = malloc(sizeof(struct mycomplex));
     struct mycomplex* c = malloc(sizeof(struct mycomplex));
-    system("clear");
-    printf("1 - Add\n");
-    printf("2 - Sub\n");
-    printf("3 - Mul\n");
-    printf("4 - Divide\n");
-    printf("5 - Quit\n");
-    printf("Choose the operation:\n");
-    char ch;
-    while(ch != '5')
+    while(n != 5)
     {
-        ch = getchar(); 
-        switch(ch)
+        void* handle;
+        void* (*func)(struct mycomplex *, struct mycomplex *, struct mycomplex *);
+        char* error;
+        printf("Choose the operation\n1 - Add\n2 - Sub\n3 - Mul\n4 - Div\n5 - Quit\n");
+        scanf("%d", &n);
+        switch(n)
         {
-            case 49:
+            case 1:
                 printf("Input A and B\n");
                 scanf("%lf %lf %lf %lf", &a->real, &a->img, &b->real, &b->img);
-                printf("A + B = "); 
-                add(a, b, c);
+                handle = dlopen("libadd.so", RTLD_LAZY);
+                if(!handle) 
+                {
+                    fputs (dlerror(), stderr);
+                    exit(1);
+                }
+                func = dlsym(handle, "add");
+                if((error = dlerror()) != NULL)  
+                {
+                    fprintf (stderr, "%s\n", error);
+                    exit(1);
+                }
+                (*func)(a, b, c);
                 printf("(%lf, %lf)\n", c->real, c->img);
-                printf("Choose the operation:\n");
+                dlclose(handle);
                 break;
-            case 50:
+            case 2:
                 printf("Input A and B\n");
                 scanf("%lf %lf %lf %lf", &a->real, &a->img, &b->real, &b->img);
-                printf("A - B = ");
-                sub(a, b, c);
+                handle = dlopen("libsub.so", RTLD_LAZY);
+                if(!handle) 
+                {
+                    fputs (dlerror(), stderr);
+                    exit(1);
+                }
+                func = dlsym(handle, "sub");
+                if((error = dlerror()) != NULL)  
+                {
+                    fprintf (stderr, "%s\n", error);
+                    exit(1);
+                }
+                (*func)(a, b, c);
                 printf("(%lf, %lf)\n", c->real, c->img);
-                printf("Choose the operation:\n");
+                dlclose(handle);
                 break;
-            case 51:
+            case 3:
                 printf("Input A and B\n");
                 scanf("%lf %lf %lf %lf", &a->real, &a->img, &b->real, &b->img);
-                printf("A * B = ");
-                mul(a, b, c);
+                handle = dlopen("libmul.so", RTLD_LAZY);
+                if(!handle) 
+                {
+                    fputs (dlerror(), stderr);
+                    exit(1);
+                }
+                func = dlsym(handle, "mul");
+                if((error = dlerror()) != NULL)  
+                {
+                    fprintf (stderr, "%s\n", error);
+                    exit(1);
+                }
+                (*func)(a, b, c);
                 printf("(%lf, %lf)\n", c->real, c->img);
-                printf("Choose the operation:\n");
+                dlclose(handle);
                 break;
-            case 52:
+            case 4:
                 printf("Input A and B\n");
                 scanf("%lf %lf %lf %lf", &a->real, &a->img, &b->real, &b->img);
                 if((b->real * b->real + b->img * b->img) == 0)
                 {
                     printf("Wrong numbers!\n");
-                    printf("Choose the operation:\n");
                     break;
                 }
                 printf("A / B = ");
-                divide(a, b, c);
+                handle = dlopen("libdiv.so", RTLD_LAZY);
+                if(!handle) 
+                {
+                    fputs (dlerror(), stderr);
+                    exit(1);
+                }
+                func = dlsym(handle, "divide");
+                if((error = dlerror()) != NULL)  
+                {
+                    fprintf (stderr, "%s\n", error);
+                    exit(1);
+                }
+                (*func)(a, b, c);
                 printf("(%lf, %lf)\n", c->real, c->img);
-                printf("Choose the operation:\n");
+                dlclose(handle);
                 break;
             default:
                 break;
         }
-    }   
-
+    }
     return 0;
-}
+} 
